8372da587289f7e3c18212c754d934fd53ae6c50
DavidValenciaCa <davidvalencia164@gmail.com>
2025-06-25 18:38:31 -0500

    Clase Docker database y .env

--- calculadora/src/app.controller.ts
+++ calculadora/src/app.controller.ts
@@ -1,9 +1,23 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ConfigService } from '@nestjs/config'; // Importing ConfigService to manage configuration settings
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly configService: ConfigService, // Injecting ConfigService to access configuration settings
+  ) {}
+
+  @Get('database')
+  getDatabaseInformation() {
+    return {
+      DB_HOST: this.configService.get('DB_HOST'),
+      DB_USER: this.configService.get('DB_USER'),
+      DB_PASSWORD: this.configService.get('DB_PASSWORD'),
+      DB_NAME: process.env.DB_NAME, // Accessing environment variable directly
+    };
+  }
 
   @Get()
   getHello(): string {
